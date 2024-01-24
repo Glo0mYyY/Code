a4ed444a356f4d14aa8d4f3901d5ee37d1f1ce43
Jan Niederhauser <jan.niederhauser@hftm.ch>
2024-01-24 17:51:18 +0100

    new loop file

--- Testing/loop.py
+++ Testing/loop.py
@@ -4,12 +4,13 @@ def factorial(n):
     if n == 0:
         return 1
     else:
-        i = 0
+        i = 1
+        factorial = 1
         #return n * factorial(n-1)
-        while i < n:
-            n = n * i
-            i += 1        
-        return n  
+        while i <= n:
+            factorial = factorial * i
+            i = i + 1        
+        return factorial  
 
 num = int(input("Geben Sie eine Zahl ein: ")) 
 if num %2 == 0:
