580ec13154a72ee24cc4bdaf4b2f3e08d7490608
Jan Niederhauser <jan.niederhauser@hftm.ch>
2024-02-14 18:16:44 +0100

    minor changes

--- Listen/A4_listOfString.py
+++ Listen/A4_listOfString.py
@@ -1,7 +1,11 @@
 woerter = ['Hallo','Hello','holla','ciao','salut','hola','hej','hei','hej']
 referenzWort = 'Halo'
+count = 0
 
 for wort in woerter:
     if (wort[0]) == referenzWort[0] and (wort[-1]) == referenzWort[-1] and len(wort) >= 2:
+        count += 1
         print(wort)
         
+
+print(count)
