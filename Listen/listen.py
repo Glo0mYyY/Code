b0d4832b727cd6eb1723b4f3ab752c4911fec683
Jan Niederhauser <jan.niederhauser@hftm.ch>
2024-02-14 16:55:48 +0100

    lists

---
+++ Listen/listen.py
@@ -0,0 +1,10 @@
+coordinates = [[0,200],[0,0],[200,0],[200,200]]
+print(coordinates)
+coordinates[0] = [100,100]
+print(coordinates)
+coordinates[1] = "Hallo"
+print(coordinates)
+coordinates.append(1000)
+print(coordinates)
+coordinates.insert(1, "Welt")
+print(coordinates)
