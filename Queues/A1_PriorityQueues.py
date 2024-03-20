3636ba2f0119ad6a7114da90e8f858d54edaca43
Jan.Niederhauser@hftm.ch <Jan.Niederhauser@hftm.ch>
2024-03-20 17:38:37 +0100

    Add PriorityQueues implementation

--- Queues/A1_PriorityQueues.py
+++ Queues/A1_PriorityQueues.py
@@ -1,20 +1,23 @@
-class stack:
+class PriorityQueue:
     def __init__(self):
         self.liste = []
 
     def is_Empty(self):
         return len(self.liste) == 0
 
-    def enqueue(self, element):
+    def enqueue(self, priority, value):
+        element = priority, value
         self.liste.append(element)
 
     def dequeue(self):
         if not self.is_Empty():
+            self.liste.sort()
             return self.liste.pop(-1)
         return None
 
     def peek(self):
         if not self.is_Empty():
+            self.liste.sort()
             return self.liste[-1]
         return None
 
@@ -22,14 +25,14 @@ class stack:
         return len(self.liste)
 
 
-queue = stack()
-queue.enqueue(2)
-queue.enqueue(1)
-queue.enqueue(3)
+queue = PriorityQueue()
+queue.enqueue(3, "Jan")
+queue.enqueue(1, "Peter")
+queue.enqueue(2, "Kevin")
+queue.enqueue(5, "John")
 
 print(queue.is_Empty())
-print(queue.dequeue())  # Liefert 3 zurück
-print(queue.dequeue())  # Liefert 1 zurück
-print(queue.peek())  # Liefert 2 zurück
-print(queue.dequeue())  # Liefert 2 zurück
-print(queue.size())  # Liefert 0 zurück
+print(queue.peek())
+print(queue.dequeue())
+print(queue.size())
+print(queue.peek())
